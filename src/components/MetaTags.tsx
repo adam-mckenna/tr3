81286157d08afd14ab2c1236787e11f5383cd297
Adam McKenna <adam-mckenna@outlook.com>
2025-06-24 23:42:03 +0200

    SEO: minor content changes

--- src/components/MetaTags.tsx
+++ src/components/MetaTags.tsx
@@ -9,13 +9,13 @@ interface MetaTagsProps {
 }
 
 export const MetaTags = ({
-  title = "Running Training & Recovery Blog | adamcantrun",
-  description = "Exploring the art and science of optimal training, racing and recovery for runners.",
+  title = "Running Science, Training & Recovery | adamcantrun",
+  description = "Exploring the art and science of optimal training, racing and recovery for runners. Tips, insights, and personal experiences from a passionate runner.",
   image,
   url = window.location.href,
   type = "website",
 }: MetaTagsProps) => {
-  const fullTitle = `${title} | adamcantrun`;
+  const fullTitle = `${title} | adamcantrun | Running Science, Training & Recovery`;
 
   return (
     <Helmet>
