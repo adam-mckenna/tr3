b05e6cbe055d7f34ee50bf1b9778c3d1562af4fd
Adam McKenna <adam-mckenna@outlook.com>
2025-06-25 17:18:27 +0200

    SEO: remove duplicate Head

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://adamcantrun.com",
+  site: "https://www.adamcantrun.com",
   integrations: [
     mdx(),
     sitemap(),
