efb9c33d099451babfa94bd2fd00ae3d96aa365d
Adam McKenna <adam-mckenna@outlook.com>
2025-07-18 18:17:52 +0200

    feat: improved index

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => (
+  <footer className="bg-chardon w-full py-12">
+    <div className="text-sm max-w-lg mx-auto flex items-center justify-center">
+      &copy; tr3 2025
+    </div>
+  </footer>
+);
