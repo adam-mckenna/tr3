30e1a01bc4e1e04b2c2f754ab110a97dbfe1e7f9
Adam McKenna <adam-mckenna@outlook.com>
2025-08-06 14:58:31 +0100

    chore: fix date format

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,5 +1,6 @@
+import { format } from "date-fns";
+
 export const getFormattedDate = (isoDate: string) => {
   const date = new Date(isoDate);
-  const pad = (n: number) => n.toString().padStart(2, "0");
-  return `${pad(date.getDate())}.${pad(date.getMonth() + 1)}.${date.getFullYear()}`;
+  return format(date, "MMMM do, yyyy");
 };
